07dea5174883551c193c7b2366b8ba5fbe2727f8
Naitik Shah <n@daaku.org>
2023-11-28 11:09:17 +0400

    modernize and fix build
    - fix build for TS 5.x
    - use vite for better test workflow
    - remove local prettier and eslint

--- test/tests.ts
+++ test/tests.ts
@@ -1,6 +1,11 @@
+import QUnit from 'qunit';
+import 'qunit/qunit/qunit.css';
 import { Message, Timestamp } from '@daaku/kombat';
 import { deleteDB, IDBPDatabase, openDB } from 'idb';
 import { customAlphabet } from 'nanoid';
+
+// @ts-ignore
+window.HARNESS_RUN_END && QUnit.on('runEnd', window.HARNESS_RUN_END);
 
 import {
   Changes,
